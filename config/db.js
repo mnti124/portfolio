48cef88b9eb720e2a2b40df4fc21b98ac3af3a7b
Michael Nti <michaelnti@Michaels-MBP-2.home>
2020-11-09 12:10:02 -0500

    added connection to cloud database MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
 module.exports = {
-    database: 'mongodb://user:pass123@ds121225.mlab.com:21225/heroku_z41s9j1g'
+    database: 'mongodb+srv://portfolioAdmin:test124@cluster0.hch8j.mongodb.net/portfolio?retryWrites=true&w=majority'
+    
+    //'mongodb://user:pass123@ds121225.mlab.com:21225/heroku_z41s9j1g'
 }
